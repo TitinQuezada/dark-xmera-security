0baaabd1974c820a705b87af66b90bf900eeedc9
Titin <joanmanueltitin@gmail.com>
2021-10-27 22:51:25 -0400

    delete swagger

--- dark-xmera-security/Startup.cs
+++ dark-xmera-security/Startup.cs
@@ -129,10 +129,9 @@ namespace dark_xmera_security
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "dark_xmera_security v1"));
             }
-
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "dark_xmera_security v1"));
 
             app.UseCors(op =>
             {
